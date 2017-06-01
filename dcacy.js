566d962a536c015bfda5aa8ebf99d536334ff0bd
Scott Good <jsgood@us.ibm.com>
2017-06-01 08:29:00 -0400

    Setting up components for first muse test

---
+++ dcacy.js
@@ -0,0 +1,19 @@
+// remove text from org
+if(typeof(dojo) != "undefined") {
+	dojo.place(
+			'<link rel="stylesheet" type="text/css" href="/files/muse-static/dcacy/homepage_demo/style.css"></link>',
+			dojo.doc.head,
+			'last');
+
+
+require("dojo/ready", function(ready){
+  var savedView = document.getElementById('savedView');
+  var navMenu = savedView.parentNode.parentNode;
+  var newDiv = document.createElement('div');
+  newDiv.className = 'lotusMenuSection';
+  newDiv.innerHTML = "Hello World!";
+  newDiv.innerHTML += '<img src="/files/muse-static/dcacy/homepage_demo/images/watson.gif">';
+  navMenu.appendChild(newDiv);
+});
+
+}
