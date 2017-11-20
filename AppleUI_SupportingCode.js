f08a5f14dbcbfd89bc5e34afd140d79b982984e2
Scott Good <scott.good@us.ibm.com>
2017-11-20 11:44:53 -0500

    Trying moving code into the js file.

--- AppleUI_SupportingCode.js
+++ AppleUI_SupportingCode.js
@@ -1,3 +1,5 @@
+var firstCommunity = 0;
+var communitiesJSON;
 function getMoreCommunities(startNum) {
     if (startNum == 'undefined') startNum = 0;
     alert('hey, that worked!');
@@ -5,4 +7,32 @@ function getMoreCommunities(startNum) {
 function temp() {
     // nothing here yet
     alert('this is temp()');
+}
+
+function buildCommunities(json) {
+    var outHTML = '';
+    for (var a = 0; a < json.length; a++) {
+        outHTML += "<a class='communityContainer' target='_blank' href='" + json[a].href + "'>";
+        outHTML += "<div class='communityIcon'>";
+        outHTML += "<img class='communityImg' src='" + json[a].iconUrl + "' />";
+        outHTML += "<div class='communityTitle'>" + json[a].title + "</div>";
+        outHTML += "</div></a>";
+    }
+    if (outHTML !== '') {
+        var headerHTML = "<h2>My top communities</h2>";
+        var header = document.createElement('div');
+        header.setAttribute('class', 'lotusSection');
+        header.setAttribute('id', 'communitiesSectionHeader');
+
+        var div = document.createElement("div");
+        div.setAttribute('id','communityDisplayArea');
+
+        outHTML += "<button onclick=getMoreCommunities()>Test me!</button>";
+
+        var rightCol = document.getElementById("lotusColRight");
+        rightCol.appendChild(header);
+        document.getElementById('communitiesSectionHeader').innerHTML = headerHTML;
+        rightCol.appendChild(div);
+        document.getElementById('communityDisplayArea').innerHTML = outHTML;
+    }
 }
